4acbd6c02a7886f7d9ff54cbf65570107744e454
dudmy <ejemal8680@gmail.com>
2017-04-07 14:46:36 +0900

    Create splash and main activity using kotlin

--- app/src/main/java/net/dudmy/splash/MainActivity.java
+++ app/src/main/java/net/dudmy/splash/MainActivity.java
@@ -2,6 +2,9 @@ package net.dudmy.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "I'm java activity!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
